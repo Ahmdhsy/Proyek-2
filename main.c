4c939d3196812eedbcc47f4b73f6871fd0bb235c
basganajaah <bandyagaadiansyah@gmail.com>
2024-03-03 21:13:12 +0700

    Update main.c

--- main.c
+++ main.c
@@ -6,31 +6,35 @@ int main()
     public_key = generatePublicKey();
     private_key = generatePrivateKey(public_key);
 
-    char *message = (char *)malloc(100 * sizeof(char)); // Allocate memory for a string
+    char *message = (char *)malloc(100 * sizeof(char)); // Alokasi memori
 
     if (message == NULL)
     {
         printf("Memory allocation failed.\n");
-        return 1; // Exit with an error code
+        return 1; // retrun true
     }
-    printf("%d", public_key);
-    printf("%d", private_key);
-    printf("Masukkan text: ");
+    
+    printf("Public key: %d\n", public_key);
+    printf("Private key: %d\n", private_key);
+    printf("Masukkan text yang ingin dienkripsi: ");
     fgets(message, 100, stdin);
 
     int size;
     int *coded = encoder(message, &size);
-    printf("Initial message:\n%s", message);
-    printf("\n\nThe encoded message(encrypted by public key)\n");
+    printf("\nInitial message:\n%s", message);
+    printf("\nThe encoded message (encrypted by public key)\n");
+    
     for (int i = 0; i < size; i++)
     {
         printf("%d", coded[i]);
     }
-    printf("\n\nThe decoded message(decrypted by private key)\n");
+    
+    printf("\n\nThe decoded message (decrypted by private key)\n");
     char *decoded = decoder(coded, size);
-    printf("%s\n", decoded);
+    printf("%s", decoded);
 
     free(coded);
     free(decoded);
+
     return 0;
 }
