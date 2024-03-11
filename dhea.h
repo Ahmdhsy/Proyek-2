62a3e29e3172af8cd52d5fb54de023361d7fd519
dheaasprla <dsd06177@gmail.com>
2024-03-12 04:25:43 +0700

    feat/dhea

---
+++ dhea.h
@@ -0,0 +1,13 @@
+#ifndef ENCRYPT_DECRYPT_H
+#define ENCRYPT_DECRYPT_H
+
+// Fungsi untuk menampilkan menu
+void menu();
+
+// Fungsi untuk enkripsi teks
+void encrypt();     // didalam encrypt ada 2 pilihan yaitu masukkan teks atau Readfile
+
+// Fungsi untuk dekripsi teks
+void decrypt();
+
+#endif /* ENCRYPT_DECRYPT_H */
