b35316bacc9be426328b447ce85c451503bcc439
RanditZX <daivaraditya36@gmail.com>
2024-05-17 05:48:55 +0700

    update lsb

--- lsb.cpp
+++ lsb.cpp
@@ -8,50 +8,64 @@
 
 #define CHANNELS 4
 
-// Function to duplicate a string
-char *my_strdup(const char *str) {
-    if (str == NULL) {
+char *my_strdup(const char *str)
+{
+    if (str == NULL)
+    {
         return NULL;
     }
 
     size_t len = strlen(str) + 1;
     char *dup_str = (char *)malloc(len);
 
-    if (dup_str != NULL) {
+    if (dup_str != NULL)
+    {
         strcpy(dup_str, str);
     }
 
     return dup_str;
 }
 
-void hideTextInPNG(const char *inputImage, const char *outputImage, const char *text) {
+void hideTextInPNG(const char *inputImage, const char *outputImage, const char *text)
+{
     int width, height, bpp;
     unsigned char *image = stbi_load(inputImage, &width, &height, &bpp, CHANNELS);
 
-    if (image == NULL) {
+    if (image == NULL)
+    {
         printf("Error loading the image.\n");
         exit(1);
     }
 
     // Use a writable buffer for text
     char *textBuffer = my_strdup(text);
-    if (textBuffer == NULL) {
+    if (textBuffer == NULL)
+    {
         printf("Error allocating memory for text buffer.\n");
+        stbi_image_free(image);
         exit(1);
     }
 
     int textIndex = 0;
-    for (int i = 0; i < width * height * CHANNELS; i++) {
-        if (textBuffer[textIndex] == '\0') {
-            break;
-        }
+    int textLength = strlen(textBuffer);
+    int totalBits = textLength * 8;
 
-        image[i] &= 0xFE; // Clear the LSB
-        image[i] |= (textBuffer[textIndex] >> 7) & 1; // Set the MSB of the text bit
-        textBuffer[textIndex] <<= 1; // Shift to the next bit
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int pixelIndex = (y * width + x) * CHANNELS;
 
-        if (i % CHANNELS == CHANNELS - 1) {
-            textIndex++;
+            for (int c = 0; c < CHANNELS; c++)
+            {
+                if (textIndex < totalBits)
+                {
+                    // Clear the LSB and set it to the corresponding bit of the text
+                    image[pixelIndex + c] &= 0xFE;
+                    image[pixelIndex + c] |= (textBuffer[textIndex / 8] >> (7 - (textIndex % 8))) & 1;
+                    textIndex++;
+                }
+            }
         }
     }
 
@@ -63,12 +77,84 @@ void hideTextInPNG(const char *inputImage, const char *outputImage, const char *
     printf("Text hidden successfully in PNG.\n");
 }
 
-int main() {
-    const char *inputImage = "anime-png-24.png";
-    const char *outputImage = "output1.png";
-    const char *textToHide = "Hello, this is a secret message!";
+char *retrieveTextFromPNG(const char *imagePath)
+{
+    int width, height, bpp;
+    unsigned char *image = stbi_load(imagePath, &width, &height, &bpp, CHANNELS);
+
+    if (image == NULL)
+    {
+        printf("Error loading the image.\n");
+        exit(1);
+    }
+
+    // Allocate memory for the text buffer
+    char *textBuffer = (char *)malloc((width * height * CHANNELS / 8) + 1);
+    if (textBuffer == NULL)
+    {
+        printf("Error allocating memory for text buffer.\n");
+        stbi_image_free(image);
+        exit(1);
+    }
+
+    int textIndex = 0;
+    char currentChar = 0;
+    int bitCount = 0;
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int pixelIndex = (y * width + x) * CHANNELS;
+
+            for (int c = 0; c < CHANNELS; c++)
+            {
+                currentChar |= (image[pixelIndex + c] & 1) << (7 - bitCount);
+                bitCount++;
+
+                if (bitCount == 8)
+                {
+                    textBuffer[textIndex++] = currentChar;
+                    if (currentChar == '\0')
+                    {
+                        textBuffer[textIndex] = '\0';
+                        stbi_image_free(image);
+                        return textBuffer;
+                    }
+                    currentChar = 0;
+                    bitCount = 0;
+                }
+            }
+        }
+    }
+
+    // Null-terminate the text buffer
+    textBuffer[textIndex] = '\0';
+
+    stbi_image_free(image);
+
+    return textBuffer;
+}
+
+int main()
+{
+    const char *inputImage = "sample1.png";
+    const char *outputImage = "output2.png";
+    const char *textToHide = "Hello";
 
     hideTextInPNG(inputImage, outputImage, textToHide);
+
+    char *hiddenText = retrieveTextFromPNG(outputImage);
+
+    printf("Hidden Text: ");
+    int i = 0;
+    for(int i; i < 50; i++) {
+        printf("%c", hiddenText[i]);
+
+    }
+    printf("\n");
+
+    free(hiddenText);
 
     return 0;
 }
