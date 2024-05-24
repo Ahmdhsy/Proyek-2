76a898ddff82fb22d070ada9f11fd7337f9220b0
dheaasprla <dsd06177@gmail.com>
2024-05-24 17:40:23 +0700

    modifikasi menu sedikit

--- menu.c
+++ menu.c
@@ -3,13 +3,15 @@
 
 void menu()
 {
-    printf("================= Menu =================\n");
-    printf("1. Encrypt Shifting & RSA \n");
-    printf("2. Decrypt Shifting & RSA \n");
-    printf("3. Encrypt Shifting & Hashing\n");
-    printf("4. Encrypt shifting & LSB\n");
-    printf("5. Decrypt shifting & LSB\n");
-    printf("6. Keluar \n");
+    printf("========================================\n");
+    printf("                Main Menu               \n");
+    printf("========================================\n");
+    printf("1. Encrypt Shifting & RSA               \n");
+    printf("2. Decrypt Shifting & RSA               \n");
+    printf("3. Encrypt Shifting & Hashing           \n");
+    printf("4. Encrypt Shifting & LSB               \n");
+    printf("5. Decrypt Shifting & LSB               \n");
+    printf("6. Exit                                 \n");
     printf("========================================\n");
 }
 
@@ -22,9 +24,10 @@ void encryptMenu(int *pilihanEncryptMenu, char *metode) {
     int result;
     do {
         printf("Metode %s yang akan digunakan: 1\n", metode);
-        /* Menampilkan menu */
+        printf("----------------------------------------\n");
         printf("[1] Masukkan Teks\n");
-        printf("[2] Read File\n");
+        printf("[2] Baca dari File\n");
+        printf("----------------------------------------\n");
         printf("Pilih menu (1/2): ");
         result = scanf("%d", pilihanEncryptMenu);
 
@@ -34,6 +37,7 @@ void encryptMenu(int *pilihanEncryptMenu, char *metode) {
         if (result == 1) { // Check if scanf successfully read an integer
             switch (*pilihanEncryptMenu) {
                 case 1:
+                    printf("Masukkan teks yang akan dienkripsi:\n");
                     break;
                 case 2:
                     printf("Membaca teks dari file untuk dienkripsi:\n");
