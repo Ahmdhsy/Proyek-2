e1dd71a2ab2240d5131442dd47a337cb1b0dc3f1
Bandyaga Adiansyah <bandyagaadiansyah@gmail.com>
2024-05-25 13:51:58 +0700

    Fixed space handle & add shifting di hashing.c

--- hashing.h
+++ hashing.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "menu.h"
+#include "shifting.h"
 
 #define MAX_LEN 1000
 
@@ -12,17 +12,18 @@ struct Node {
     char data;
     struct Node* next;
     struct Node* prev;
-};
+} Node;
 
-void customEncrypt(const char *plaintextHash, char *ciphertextHash, int *randomValues);
-void customDecrypt(const char *ciphertextHash, char *plaintextHash, const int *randomValues);
+void customEncrypt(const char *plaintextHash, char *ciphertextHash, int *randomValues, const char *replacementTable);
+void customDecrypt(const char *ciphertextHash, char *plaintextHash, const int *randomValues, const char *replacementTable, const char *spaceSymbols, int numSymbols);
 struct Node* createNode(char data);
 void freeLinkedList(struct Node* head);
 struct Node* arrayToLinkedList(const char *array);
 void linkedListToArray(struct Node* head, char *array);
 void printLinkedList(struct Node* head);
-void addRandomCharacter(struct Node* head);
+void addRandomCharacter(struct Node* head, const char *replacementTable);
 void deleteRandomCharacter(struct Node* head);
+char replaceSpace();
 int menuHashing();
 
 #endif
